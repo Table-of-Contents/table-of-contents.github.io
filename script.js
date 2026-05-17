492c8871bcc54bf84629902ff10163a0828c1dff
mslt <masaltzero2@gmail.com>
2026-05-16 22:33:17 -0700

    Refactor book display logic and improve formatting

--- script.js
+++ script.js
@@ -7,27 +7,55 @@ fetch(URL)
     .then(res => res.json())
     .then(data => {
 
-        data.sort((a, b) => Number(b.avg_rating) - Number(a.avg_rating));
+        data.sort((a, b) =>
+            Number(b.avg_rating) - Number(a.avg_rating)
+        );
 
         const booksDiv = document.getElementById("books");
-        
-        data.forEach(book => {
+
+        data.forEach((book, index) => {
 
             const div = document.createElement("div");
 
             div.className = "book";
 
-        div.innerHTML = `
-            <h2>${book.title}</h2>
-            
-            <p><strong>ID:</strong> ${book.book_id}</p>
-            
-            <p><strong>Author:</strong> ${book.author}</p>
-            
-            <p><strong>Average Rating:</strong> ${book.avg_rating}</p>
-            
-            <p><strong>Votes:</strong> ${book.votes}</p>
-        `;
+            div.innerHTML = `
+                <div class="rank">
+                    #${index + 1}
+                </div>
+
+                <img
+                    class="cover"
+                    src="${book.cover_link}"
+                    alt="${book.title}"
+                >
+
+                <div class="info">
+
+                    <h2>${book.title}</h2>
+
+                    <p class="author">
+                        ${book.author}
+                    </p>
+
+                    <p class="rating">
+                        ★ ${book.avg_rating}
+                    </p>
+
+                </div>
+
+                <div class="side-info">
+
+                    <p class="book-id">
+                        ID: ${book.book_id}
+                    </p>
+
+                    <p class="votes">
+                        Votes: ${book.votes}
+                    </p>
+
+                </div>
+            `;
 
             booksDiv.appendChild(div);
 
